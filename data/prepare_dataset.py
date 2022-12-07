3fc02d787088d70df44f30379d1ead61e70cfdd3
Benedictus-Kent Rachmat <kent@mandiratravel.com>
2022-12-07 15:04:05 +0100

    update env and prepaare_dataset

--- data/prepare_dataset.py
+++ data/prepare_dataset.py
@@ -1,22 +1,48 @@
+# necessary libraries
+import os
 import csv
-from pathlib import Path
-
 import numpy as np
 import pandas as pd
-import os
+from pathlib import Path
+from dotenv import load_dotenv
 from skimage.io import imsave, imread
 from sklearn import decomposition, metrics, model_selection, naive_bayes, pipeline
 
+# public variables
+dotenv_path = Path('../public.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+D_PATH = os.getenv('NEW_DATA')
+DATA_PATH = os.getenv('PUBLIC_DATA')
+DATA_NAME = "Areal"
+DATA_SETS = ["train", "valid", "test"]
+
 def number_lines(fname):
+    """
+    returns the number of lines in a file 
+    :param fname: file name
+    """
     with open(fname) as f:
         return sum(1 for l in f)
 
 def fast_import(arr, fpath):
+    """
+    fill each row with the value of file fpath
+    :param arr: the array to be filled
+    :param fpath: the array to be filled
+    """
     with open(fpath) as f:
         for i, row in enumerate(csv.reader(f, delimiter=" ")):
             arr[i] = row
 
 def convert_to_torch(D_PATH, xs, ys, labels_df):
+    """
+    create an image based on the given data xs, ys, and labels_df to the designated path D_PATH
+    :param D_PATH: path to the new data
+    :param xs: raw data
+    :param ys: associated label
+    :param labels_df: the label names
+    """
     for key in xs.keys():
       os.makedirs(D_PATH, exist_ok=True)
       data_dir = os.path.join(D_PATH, key)
@@ -32,16 +58,14 @@ def convert_to_torch(D_PATH, xs, ys, labels_df):
         os.makedirs(class_dir, exist_ok=True)
         imsave("{}/{}_{}.png".format(class_dir,key,i), np.uint8(img))
 
-D_PATH = Path("../data/new_data")
-DATA_PATH = Path("../data/public_data")
-DATA_NAME = "Areal"
-DATA_SETS = ["train", "valid", "test"]
-def fmain(D_PATH, DATA_PATH, DATA_NAME, DATA_SETS):
-
-    num_fts = number_lines(DATA_PATH / f"{DATA_NAME}_feat.name")
+def fmain():
+    """
+    load, split the data and convert it to images
+    """
+    num_fts = number_lines(DATA_PATH + f"/{DATA_NAME}_feat.name")
 
     num = {
-        data_set: number_lines(DATA_PATH / f"{DATA_NAME}_{data_set}.data")
+        data_set: number_lines(DATA_PATH + f"/{DATA_NAME}_{data_set}.data")
         for data_set in DATA_SETS
     }
 
@@ -53,15 +77,15 @@ def fmain(D_PATH, DATA_PATH, DATA_NAME, DATA_SETS):
     for data_set in DATA_SETS:
         fast_import(
             xs_raw[data_set],
-            fpath=DATA_PATH / f"{DATA_NAME}_{data_set}.data"
+            fpath=DATA_PATH + f"/{DATA_NAME}_{data_set}.data"
         )
 
     labels_df = pd.read_csv(
-        DATA_PATH / f"{DATA_NAME}_label.name", header=None, names=["name"]
+        DATA_PATH + f"/{DATA_NAME}_label.name", header=None, names=["name"]
     )
 
     ys_df = pd.read_csv(
-        DATA_PATH / f"{DATA_NAME}_train.solution", header=None, names=["value"]
+        DATA_PATH + f"/{DATA_NAME}_train.solution", header=None, names=["value"]
     )
 
     ys_raw = ys_df.values.squeeze()
